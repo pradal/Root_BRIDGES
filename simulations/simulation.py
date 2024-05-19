316e89c8ef99bd59ee347eb27e91af9940a0c8e8
Tristan Gérault <tristan.gerault@inrae.fr>
2024-05-19 11:14:04 +0200

    limited max number of processes for SA

--- simulations/simulation.py
+++ simulations/simulation.py
@@ -1,5 +1,5 @@
 # Public packages
-import os, sys
+import os, sys, time
 import multiprocessing as mp
 # Model packages
 from root_bridges.root_bridges import Model
@@ -17,7 +17,7 @@ def single_run(scenario, outputs_dirpath="outputs"):
                     logging_period_in_hours=3,
                     recording_images=True, recording_off_screen=True, plotted_property="C_hexose_root", show_soil=True,
                     recording_mtg=False,
-                    recording_raw=True,
+                    recording_raw=False,
                     recording_sums=True,
                     recording_performance=True,
                     echo=True)
@@ -43,9 +43,16 @@ def single_run(scenario, outputs_dirpath="outputs"):
 
 
 def simulate_scenarios():
-    scenarios = ms.from_table(file_path="inputs/Scenarios.xlsx", which=["RB1"])
+    scenarios = ms.from_table(file_path="inputs/Scenarios_24_05.xlsx", which=["Fischer_1"])
     processes = []
+    max_processes = mp.cpu_count()
     for scenario_name, scenario in scenarios.items():
+        while len(processes) == max_processes:
+            for proc in processes:
+                if not proc.is_alive():
+                    processes.remove(proc)
+            time.sleep(1)
+
         print(f"[INFO] Launching scenario {scenario_name}...")
         p = mp.Process(target=single_run, kwargs=dict(scenario=scenario, outputs_dirpath=os.path.join("outputs", str(scenario_name))))
         p.start()
